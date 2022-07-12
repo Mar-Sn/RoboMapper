ecc252efa30cd468a869ffa62ffff761b8d4d980
Martin <martin2broers@gmail.com>
2022-07-12 18:23:16 +0200

    test for non existing fields parser

---
+++ Test/ParserPointToNonExistingField.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Shared.CustomParser;
+
+namespace Test;
+
+public class ParserPointToNonExistingField
+{
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        SetupMapper.Instance.Init();
+    }
+
+    [Test]
+    public void ExpectFailureOnNonExistingFields()
+    {
+        try
+        {
+            RoboMapper.RoboMapper.Bind<MappedA, MappedB>(e =>
+            {
+                e.MapWith<NotMappedParser>("NonExistingField1", "NonExistingField2");
+            });
+            Assert.Fail();
+        }
+        catch (Exception e)
+        {
+            Assert.Pass();
+        }
+    }
+}
