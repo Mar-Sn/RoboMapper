05db4de800ead0af761c8d999ac3b962983d4c27
Martin <martin2broers@gmail.com>
2022-07-12 18:22:55 +0200

    dupe parser should work test case

---
+++ Test/DuplicateParserDeclare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using Shared.CustomParser;
+
+namespace Test;
+
+public class DuplicateParserDeclare
+{
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        SetupMapper.Instance.Init();
+    }
+
+    [Test]
+    public void TestAddingParserAgain()
+    {
+        try
+        {
+            RoboMapper.RoboMapper.Bind<MappedA, MappedB>(e =>
+            {
+                e.MapWith<NotMappedParser>(nameof(MappedA.NotMappedA), nameof(MappedB.NotMappedB));
+            });
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(e.Message);
+        }
+        Assert.Pass();
+    }
+}
